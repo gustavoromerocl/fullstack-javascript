cb1a0a7e012d19ce191a5d451aee771a1751770d
romerodevelopments <gandresrp@gmail.com>
2022-06-07 23:24:06 -0400

    E6 y POO - desafio creando y modificando objetos primer completado

--- 3-programacion-avanzada-en-javascript/desafio-creando-y-modificando-objetos/assets/js/main.js
+++ 3-programacion-avanzada-en-javascript/desafio-creando-y-modificando-objetos/assets/js/main.js
@@ -126,7 +126,7 @@ Consultorio.prototype.agregarPaciente = function (paciente) {
 
 //método que busca los datos de los usuarios por nombre
 Consultorio.prototype.buscarPaciente = function (nombre) {
-  let paciente = this.pacientes.find(paciente => paciente.getNombre() === nombre);
+  let paciente = this.pacientes.find(paciente => paciente.getNombre().toLowerCase() === nombre.toLowerCase());
   if (paciente) return ({
     nombre: paciente.getNombre(),
     edad: paciente.getEdad(),
@@ -137,6 +137,7 @@ Consultorio.prototype.buscarPaciente = function (nombre) {
   return -1;
 }
 
+//método que retorna los datos de los pacientes
 Consultorio.prototype.mostrarPacientes = function () {
   let pacientes = this.pacientes.map(element => ({
     nombre: element.getNombre(),
@@ -179,31 +180,43 @@ console.log(consultorio.buscarPaciente("MANUEL GODOY"));
 console.log(consultorio.buscarPaciente("Obi-Wan Kenobi"));
 console.log(consultorio.mostrarPacientes());
 
+
+/* ******************************************************************************* */
+
 /* Poblando el HTML */
 
+//Se inicializa variable que alamcenará el cuerpo de la tabla
+let cuerpoArreglo = "";
+
+//Función que recicla la labor de renderizar la tabla en distintas secciones de la app
 function crearTabla(contenedor, selector, tabla, data) {
   document.querySelector(contenedor).innerHTML = "";
   document.querySelector(contenedor).innerHTML = tabla;
   document.querySelector(selector).innerHTML = data.join("");  
 }
 
+// Funcion que recicla la actualización del cuerpo de la tabla
+function actualizarTabla(){
+  cuerpoArreglo = consultorio.mostrarPacientes().map(function (elemento) {
+    return `
+    <tr class="odd:bg-gray-100">
+      <td>${elemento.nombre}</td>
+      <td>${elemento.edad}</td>
+      <td>${elemento.rut}</td>
+      <td>${elemento.diagnostico}</td>
+    </tr>
+    `
+  });
+}
+
+//Arrreglo con los encabezados de la tabla
 let encabezadosArray = Object.keys(...consultorio.mostrarPacientes());
 
-let cuerpoArreglo = consultorio.mostrarPacientes().map(function (elemento) {
-  return `
-  <tr class="odd:bg-gray-100">
-    <td>${elemento.nombre}</td>
-    <td>${elemento.edad}</td>
-    <td>${elemento.rut}</td>
-    <td>${elemento.diagnostico}</td>
-  </tr>
-  `
-});
-
+//Inyección de la tabla al html
 let tabla = `
   <section>
     <!-- tabla de pacientes -->
-    <table class=" table-auto mx-auto w-full">
+    <table class="table-auto border-collapse mx-auto w-full mt-6">
       <thead>
         <tr class="bg-stone-600 text-white">
           <th class="text-left pr-3 rounded-tl-md">${encabezadosArray[0]}</th>
@@ -219,55 +232,67 @@ let tabla = `
   </section>
 `; 
 
-
+//Inyección del formulario de busqueda al html
 let formularioBusqueda = `
-  <form id="formulario-busqueda" class="flex flex-col mx-auto mb-9 w-96 py-12 rounded border-2 border-gray-500">
-    <div class="w-64 mx-auto">
-      <label for="paciente" >Nombre paciente:</label>
-      <input type="text" id="paciente" placeholder="Ej: Gustavo Romero" class="w-64 rounded-md py-2  mb-3  border-2 border-gray-300">
-      <p class="text-red-400"></p>
-    </div>
-    <button class="w-32 py-2 px-4 bg-black text-white mx-auto rounded-md">Buscar</button>
-  </form>
-  <div id="resultado-busqueda"></div>
-
+  <section class="flex flex-col justify-start items-center">
+    <form id="formulario-busqueda" class="flex flex-col mx-auto sm:mb-9 sm:w-96 py-12 rounded">
+      <div class="w-64 mx-auto">
+        <label for="paciente" >Nombre paciente:</label>
+        <input type="text" id="paciente" placeholder="Ej: Gustavo Romero" class="w-64 rounded-md py-2  mb-3  border-2 border-gray-300" required>
+        <p class="text-red-400 pb-2"></p>
+      </div>
+      <button class="w-32 py-2 px-4 bg-black text-white mx-auto rounded-md">Buscar</button>
+    </form>
+    <div id="resultado-busqueda"></div>
+  </section>
 `;
 
+//Inyección del formulario de paciente al html
 let formularioPaciente = `
-  <form id="formulario-paciente" class="mx-auto flex w-96 flex-col rounded border-2 border-gray-500 py-12">
-    <div class="mx-auto w-64">
-      <label for="paciente">Nombre:</label>
-      <input type="text" id="nombre" placeholder="Ej: Gustavo Romero" class="mb-3 w-64 rounded-md border-2 border-gray-300 py-2" />
-      <label for="paciente">Edad:</label>
-      <input type="text" id="edad" placeholder="Ej: 30" class="mb-3 w-64 rounded-md border-2 border-gray-300 py-2" />
-      <label for="paciente">Rut:</label>
-      <input type="text" id="rut" placeholder="Ej: 17690xxx-x" class="mb-3 w-64 rounded-md border-2 border-gray-300 py-2" />
-      <label for="paciente">Diagnóstico:</label>
-      <input type="text" id="diagnostico" placeholder="Ej: Covid positivo" class="mb-3 w-64 rounded-md border-2 border-gray-300 py-2" />
-    </div>
-    <button class="mx-auto w-32 rounded-md bg-black py-2 px-4 text-white">Agregar</button>
-  </form>
+  <section class="flex flex-col justify-around items-center" >
+    <form id="formulario-paciente" class="flex sm:w-96 flex-col py-12">
+      <div class="mx-auto w-64">
+        <label for="paciente">Nombre:</label>
+        <input type="text" id="nombre" placeholder="Ej: Gustavo Romero" class="mb-3 w-64 rounded-md border-2 border-gray-300 py-2" required/>
+        <p class="text-red-400 pb-2"></p>
+        <label for="paciente">Edad:</label>
+        <input type="text" id="edad" placeholder="Ej: 30" class="mb-3 w-64 rounded-md border-2 border-gray-300 py-2" required/>
+        <p class="text-red-400 pb-2"></p>
+        <label for="paciente">Rut:</label>
+        <input type="text" id="rut" placeholder="Ej: xxxxxxxx-x" class="mb-3 w-64 rounded-md border-2 border-gray-300 py-2" required/>
+        <p class="text-red-400 pb-2"></p>
+        <label for="paciente">Diagnóstico:</label>
+        <input type="text" id="diagnostico" placeholder="Ej: Covid positivo" class="mb-3 w-64 rounded-md border-2 border-gray-300 py-2" required/>
+        <p class="text-red-400 pb-2"></p>
+      </div>
+      <button class="mx-auto w-32 rounded-md bg-black py-2 px-4 mb-6 text-white">Agregar</button>
+    </form>
+  </section>
+
 `;
+
+
+/* Listeners */
 
 document.addEventListener("DOMContentLoaded", function () {
   //console.log("documento listo");
+  actualizarTabla();
   crearTabla("#main", "#cuerpo", tabla, cuerpoArreglo);
 })
 
-
+//Dashboard inicio
 document.querySelector('#inicio').addEventListener("click", function (ev) {
   ev.preventDefault();
   crearTabla("#main", "#cuerpo", tabla, cuerpoArreglo);
-
 });
 
-//Buscar un paciente
+//Dashboard buscar un paciente
 document.querySelector('#buscar').addEventListener("click", function (ev) {
   ev.preventDefault();
   //Renderizamos el formulario de busqueda
   document.querySelector('#main').innerHTML = formularioBusqueda;
   
-  //Agregamos listener al envío del formulario
+  //Listener del formulario de busqueda de un paciente
   document.querySelector("#formulario-busqueda").addEventListener("submit", function(ev){
     ev.preventDefault();
     //guardamos la busqueda recibida en el input en una variable
@@ -296,40 +321,43 @@ document.querySelector('#buscar').addEventListener("click", function (ev) {
 });
 
 
-//Agregar un paciente
+//Dashboard agregar un paciente
 document.querySelector('#agregar').addEventListener("click", function (ev) {
   ev.preventDefault();
   
   //Renderiza el formulario
   document.querySelector('#main').innerHTML = formularioPaciente;
-  console.log("me estoy ejecutando");
+  //console.log("me estoy ejecutando");
 
-  let formulario =  document.querySelector("#formulario-paciente");
+  let formulario = document.querySelector("#formulario-paciente");
 
+  //Listener del submit del formulario de agregar paciente
   formulario.addEventListener("submit", function(ev){
     ev.preventDefault();
-    let nombre = formulario.querySelector("#nombre").value;
-    let edad = formulario.querySelector("#edad").value;
-    let rut = formulario.querySelector("#rut").value;
-    let diagnostico = formulario.querySelector("#diagnostico").value;
+    let nombre = formulario.querySelector("#nombre");
+    let edad = formulario.querySelector("#edad");
+    let rut = formulario.querySelector("#rut");
+    let diagnostico = formulario.querySelector("#diagnostico");
+    let parrafo = formulario.querySelectorAll("p");
 
-    let newPaciente = new Paciente(nombre, edad, rut, diagnostico);
+    //Limpiamos los parrafos del formulario
+    parrafo.forEach(parrafo => parrafo.innerHTML = "");
+
+    //Validaciones
+    if(!isNaN(nombre.value)) return nombre.nextElementSibling.innerHTML = "No ingresaste un nombre válido";
+    if(isNaN(edad.value)) return edad.nextElementSibling.innerHTML = "No ingresaste una edad válida";
+    //if(isNaN(rut.value)) return rut.nextElementSibling.innerHTML = "No ingresaste un rut válido";
+    if(!isNaN(diagnostico.value)) return diagnostico.nextElementSibling.innerHTML = "No ingresaste una diagnostico válido";
+
+
+    let newPaciente = new Paciente(nombre.value.toUpperCase(), edad.value, rut.value, diagnostico.value);
     
     consultorio.agregarPaciente(newPaciente);
     console.log(consultorio);
 
-    let cuerpoNuevoArreglo = consultorio.mostrarPacientes().map(function (elemento) {
-      return `
-        <tr class="odd:bg-gray-100">
-          <td>${elemento.nombre}</td>
-          <td>${elemento.edad}</td>
-          <td>${elemento.rut}</td>
-          <td>${elemento.diagnostico}</td>
-        </tr>
-      `
-    });
+    actualizarTabla();
   
-    crearTabla("#main", "#cuerpo", tabla, cuerpoNuevoArreglo);
+    crearTabla("#main", "#cuerpo", tabla, cuerpoArreglo);
   });
   
 });
